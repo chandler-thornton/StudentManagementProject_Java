3091b55ab3d71c51f385ff80fe1d3b92e22d4e30
chandler-thornton <chanman1218@outlook.com>
2021-08-02 18:07:04 -0400

    Implemented GUI for login system, Added listener for login click

--- Student Management System/src/studentManagementSystem/StudentManagementApp.java
+++ Student Management System/src/studentManagementSystem/StudentManagementApp.java
@@ -1,13 +1,62 @@
 package studentManagementSystem;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class StudentManagementApp {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class StudentManagementApp implements ActionListener{
 	
-	public static ArrayList<Student> students = new ArrayList<Student>();
+	private static ArrayList<Student> students = new ArrayList<Student>();
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton loginButton;
 
 	public static void main(String[] args) {
+		//JFrame
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setSize(275, 175);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		
+		//User
+		userLabel = new JLabel("User");
+		userLabel.setBounds(15, 15, 80, 25);
+		panel.add(userLabel);
+		
+		userText = new JTextField(25);
+		userText.setBounds(80, 18, 165, 20);
+		panel.add(userText);
+		
+		//Password
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(15, 55, 80, 25);
+		panel.add(passwordLabel);
+		
+		passwordText = new JPasswordField(25);
+		passwordText.setBounds(80, 58, 165, 20);
+		panel.add(passwordText);
+		
+		//Login 
+		loginButton = new JButton("Login");
+		loginButton.setBounds(168, 100, 75, 25);
+		loginButton.addActionListener(new StudentManagementApp());
+		panel.add(loginButton);
+		
+		frame.setVisible(true);
+		
 		//Ask for number of new students
 		Scanner in = new Scanner(System.in);
 		
@@ -25,6 +74,12 @@ public class StudentManagementApp {
 			System.out.println();
 		}
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String userInput = userText.getText();
+		String passwordInput = passwordText.getText();
 	}
 
 }
